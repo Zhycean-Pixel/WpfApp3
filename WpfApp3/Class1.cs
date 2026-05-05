0db60113b9c707c51fe4d48fba955e3c83640bb9
Zhycean-Pixel <zhengsyz852@gmail.com>
2026-05-05 17:02:24 +0800

    添加了一个类

---
+++ WpfApp3/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp3
+{
+    internal class Class1
+    {
+        int a = 1;
+
+        string b = "1";
+    }
+}
